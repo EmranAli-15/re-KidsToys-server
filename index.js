9d08ef7edc3530443badae27b424d337f6a8ff91
EmranAli-15 <mohammademranali15@gmail.com>
2023-05-18 10:06:36 +0600

    make a json for gallery data

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const popular = require('./data/popular.json');
+
 app.get('/', (req, res) => {
     res.send('Hello Toys');
+})
+app.get('/popular', (req, res) => {
+    res.send(popular);
 })
 app.listen(port, () => {
     console.log(`This port is running on port : ${port}`);
